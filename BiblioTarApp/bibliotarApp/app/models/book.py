f997d4d948da06f4bde5a12b13eee52bdaf8c0fa
MartosRoderik <roderikmartos@gmail.com>
2026-03-05 18:11:17 +0100

    első bemutató tényleges fixek

--- BiblioTarApp/bibliotarApp/app/models/book.py
+++ BiblioTarApp/bibliotarApp/app/models/book.py
@@ -6,6 +6,7 @@ from typing import List, Optional
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.types import String, Integer, Boolean
 from sqlalchemy import ForeignKey
+from datetime import date
 
 
 
@@ -19,5 +20,9 @@ class Book(db.Model):
     available : Mapped[bool]
     status : Mapped[str] = mapped_column(String(30))
 
+    daysBorrowed : Mapped[int]
+    dateBorrowed : Mapped[date]
+
+    borrowedBooks : Mapped[List["borrowedBook"]] = relationship(back_populates="book", lazy=True)
 
 
